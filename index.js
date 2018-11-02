f26921c7e766e8e085671210c054a80cc6d4dada
Brad Buchanan <bradley.c.buchanan@gmail.com>
2018-11-02 08:34:48 -0700

    Render multiple dancers

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const processDancer = require('./src/processDancer');
 
 
 (async function main() {
+  await processDancer('bear');
   await processDancer('cat');
+  await processDancer('dog');
+  await processDancer('duck');
+  await processDancer('frog');
+  await processDancer('moose');
+  await processDancer('robot');
+  await processDancer('unicorn');
 }());

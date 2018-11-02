3e29755993eeed9c549f50c1bc38fa56dd913acc
Brad Buchanan <bradley.c.buchanan@gmail.com>
2018-11-02 08:37:28 -0700

    Render multiple dancers at once

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 const processDancer = require('./src/processDancer');
 
-
-(async function main() {
-  await processDancer('bear');
-  await processDancer('cat');
-  await processDancer('dog');
-  await processDancer('duck');
-  await processDancer('frog');
-  await processDancer('moose');
-  await processDancer('robot');
-  await processDancer('unicorn');
-}());
+const dancers = ['bear', 'cat', 'dog', 'duck', 'frog', 'moose', 'robot', 'unicorn'];
+Promise.all(dancers.map(processDancer));
